f0754250cacff5a835880adc7793bfa545814b19
Wendelin Gerein <info@webdelin.de>
2021-10-17 14:02:38 +0200

    Clinicjs Tast analyse Checked

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # NestJs Typescript + MongoDB RESTAPI Server
+
+## Debuging
+
+### Autocannon
+
+npm i -g autocannon
+autocannon http://localhost:3000/api/review/byProduct/12d031a3-ac5c-4200-9345-fdfb86398f74
+
+┌─────────┬───────┬───────┬───────┬───────┬──────────┬─────────┬───────┐
+│ Stat │ 2.5% │ 50% │ 97.5% │ 99% │ Avg │ Stdev │ Max │
+├─────────┼───────┼───────┼───────┼───────┼──────────┼─────────┼───────┤
+│ Latency │ 23 ms │ 25 ms │ 41 ms │ 47 ms │ 26.24 ms │ 4.56 ms │ 62 ms │
+└─────────┴───────┴───────┴───────┴───────┴──────────┴─────────┴───────┘
+┌───────────┬────────┬────────┬────────┬────────┬────────┬─────────┬────────┐
+│ Stat │ 1% │ 2.5% │ 50% │ 97.5% │ Avg │ Stdev │ Min │
+├───────────┼────────┼────────┼────────┼────────┼────────┼─────────┼────────┤
+│ Req/Sec │ 339 │ 339 │ 378 │ 389 │ 373.5 │ 14.27 │ 339 │
+├───────────┼────────┼────────┼────────┼────────┼────────┼─────────┼────────┤
+│ Bytes/Sec │ 104 kB │ 104 kB │ 116 kB │ 119 kB │ 114 kB │ 4.37 kB │ 104 kB │
+└───────────┴────────┴────────┴────────┴────────┴────────┴─────────┴────────┘
+
+### Clinicjs
+
+https://clinicjs.org/documentation/doctor/03-first-analysis/
+
+npm i -g clinic
+
+npm run buid
+
+clinic doctor --on-port 'autocannon localhost:$PORT//api/review/byProduct/12d031a3-ac5c-4200' -- node dist/main.js
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -5,23 +5,37 @@ import { InjectModel } from 'nestjs-typegoose';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewModel } from './review.model';
 
+class Leak {}
+
+const leaks = [];
+
 @Injectable()
 export class ReviewService {
-	constructor(@InjectModel(ReviewModel) private readonly reviewModel: ModelType<ReviewModel>) { }
+  constructor(
+    @InjectModel(ReviewModel)
+    private readonly reviewModel: ModelType<ReviewModel>,
+  ) {}
 
-	async create(dto: CreateReviewDto): Promise<DocumentType<ReviewModel>> {
-		return this.reviewModel.create(dto);
-	}
+  async create(dto: CreateReviewDto): Promise<DocumentType<ReviewModel>> {
+    return this.reviewModel.create(dto);
+  }
 
-	async delete(id: string): Promise<DocumentType<ReviewModel> | null> {
-		return this.reviewModel.findByIdAndDelete(id).exec();
-	}
+  async delete(id: string): Promise<DocumentType<ReviewModel> | null> {
+    return this.reviewModel.findByIdAndDelete(id).exec();
+  }
 
-	async findByProductId(productId: string): Promise<DocumentType<ReviewModel>[]> {
-		return this.reviewModel.find({ productId: new Types.ObjectId(productId) }).exec();
-	}
+  async findByProductId(
+    productId: string,
+  ): Promise<DocumentType<ReviewModel>[]> {
+    leaks.push(new Leak());
+    return this.reviewModel
+      .find({ productId: new Types.ObjectId(productId) })
+      .exec();
+  }
 
-	async deleteByProductId(productId: string) {
-		return this.reviewModel.deleteMany({ productId: new Types.ObjectId(productId) }).exec();
-	}
+  async deleteByProductId(productId: string) {
+    return this.reviewModel
+      .deleteMany({ productId: new Types.ObjectId(productId) })
+      .exec();
+  }
 }
