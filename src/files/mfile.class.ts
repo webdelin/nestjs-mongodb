0860f548ffdc63be45bdebc6ebd721607fde0ac5
Wendelin Gerein <info@webdelin.de>
2021-10-20 23:07:26 +0200

    file upload and sitemap generate success

---
+++ src/files/mfile.class.ts
@@ -0,0 +1,8 @@
+export class MFile {
+	originalname: string;
+	buffer: Buffer;
+	constructor(file: Express.Multer.File | MFile ) {
+		this.originalname = file.originalname;
+		this.buffer = file.buffer;
+	}
+}
