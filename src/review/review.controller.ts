e1e2a01fe284209fa2e079f65c27f5a8be4b2fe9
Wendelin Gerein <info@webdelin.de>
2021-10-21 17:27:16 +0200

    telegram bot success

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -16,16 +16,31 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { ReviewService } from './review.service';
 import {IdValidationPipe} from '../pipes/id-validation.pipe';
+import {TelegramService} from '../telegram/telegram.service';
 
 @Controller('review')
 export class ReviewController {
-  constructor(private readonly reviewService: ReviewService) {}
+  constructor(
+	  private readonly reviewService: ReviewService,
+	  private readonly telegramService: TelegramService
+  ) {}
 
   @UsePipes(new ValidationPipe())
   @Post('create')
   async create(@Body() dto: CreateReviewDto) {
 	return this.reviewService.create(dto);
   }
+
+	@UsePipes(new ValidationPipe())
+	@Post('notify')
+	async notify(@Body() dto: CreateReviewDto) {
+	  const message = `Name: ${dto.name}\n`
+		+ `Title: ${dto.title}\n`
+		+ `Beschreibung: ${dto.description}\n`
+		  + `Bewertung: ${dto.rating}\n`
+		+ `Produkt ID: ${dto.productId}\n`;
+		return this.telegramService.sendMessage(message);
+	}
 
   @Get('byProduct/:productId')
   async getByProduct(@Param('productId', IdValidationPipe) productId: string) {
